9b6c006493111e590e11ff160abbe263c05ec16c
server <cephei.1313@gmail.com>
2017-01-19 00:35:35 -0300

    the requests to database consider a device identifier

--- beacon/views.py
+++ beacon/views.py
@@ -2,12 +2,13 @@ from django.shortcuts import render
 from django.views.generic import View
 from django.http import JsonResponse
 from django.db import connection
+from django.db.models.query import Prefetch
 from django.utils import timezone, dateparse
 
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 
-from beacon.models import Beacon, BeaconLog, Event
+from beacon.models import DetectorDevice, Beacon, BeaconLog, Event
 
 import json
 
@@ -18,9 +19,9 @@ class GetChartData(View):
     def __init__(self):
         self.context={}
 
-    def getC3EventsData(self, start, end):
+    def getC3EventsData(self, start, end, deviceId):
 
-        events = GetEventsData().getEvents(start, end)
+        events = GetEventsData().getEvents(start, end, deviceId)
         eventsWithAttrNameChanged = []
         for event in events:
             eventsWithAttrNameChanged.append({
@@ -30,28 +31,29 @@ class GetChartData(View):
             })
         return eventsWithAttrNameChanged
 
-    def getC3Data(self, start, end):
+    def getC3Data(self, start, end, deviceId):
         
         AXIS_PREFIX = 'axis_'
 
-        records = BeaconLog.objects.filter(time__range=(start, end))
+        records = BeaconLog.objects.filter(
+                time__range=(start, end), device__externalId=deviceId)
          
         data = {}
         devices = set()
         xs = {}
         for record in records:
             beacon = record.beacon.getDict()
-            deviceId = beacon['uuid'] + '-' + beacon['major'] + '-' + beacon['minor']
+            beaconId = beacon['uuid'] + '-' + beacon['major'] + '-' + beacon['minor']
 
-            if deviceId not in devices:
-                devices.add(deviceId)
-                data[AXIS_PREFIX + deviceId] = []
-                data[deviceId] = []
-                xs[deviceId] = AXIS_PREFIX + deviceId
+            if beaconId not in devices:
+                devices.add(beaconId)
+                data[AXIS_PREFIX + beaconId] = []
+                data[beaconId] = []
+                xs[beaconId] = AXIS_PREFIX + beaconId
 
             recordFormatted = record.getDict()
-            data[AXIS_PREFIX + deviceId].append(recordFormatted['time'])
-            data[deviceId].append(recordFormatted['rssi'])
+            data[AXIS_PREFIX + beaconId].append(recordFormatted['time'])
+            data[beaconId].append(recordFormatted['rssi'])
 
         dataWithName = []
         for key in data.keys():
@@ -62,8 +64,9 @@ class GetChartData(View):
 
     def get(self, request):
  
-        start = request.GET['start']
-        end   = request.GET['end']
+        start    = request.GET['start']
+        end      = request.GET['end']
+        deviceId = request.GET['device']
 
         start = dateparse.parse_datetime(start)
         start = timezone.make_aware(start)
@@ -71,8 +74,8 @@ class GetChartData(View):
         end   = timezone.make_aware(end)
         
         response = {}
-        response['xs'], response['columns'] = self.getC3Data(start, end)
-        response['events'] = self.getC3EventsData(start, end)
+        response['xs'], response['columns'] = self.getC3Data(start, end, deviceId)
+        response['events'] = self.getC3EventsData(start, end, deviceId)
 
         return JsonResponse(response, safe=False)
 
@@ -82,26 +85,28 @@ class GetEventsData(View):
     def __init__(self):
         self.context={}
 
-    def getEvents(self, start, end):
+    def getEvents(self, start, end, deviceId):
  
-        events = Event.objects.filter(time__range=(start, end))
+        events = Event.objects.filter(time__range=(start, end), device__externalId=deviceId)
         eventList = map(lambda x: x.getDict(), events)
 
         return eventList
 
     def get(self, request):
 
-        start = request.GET['start']
-        end   = request.GET['end']
+        start    = request.GET['start']
+        end      = request.GET['end']
+        deviceId = request.GET['device']
 
         start = dateparse.parse_datetime(start)
         start = timezone.make_aware(start)
         end   = dateparse.parse_datetime(end)
         end   = timezone.make_aware(end)
 
-        response = self.getEvents(start, end)
+        response = self.getEvents(start, end, deviceId)
 
         return JsonResponse(response, safe=False)
+
 
 class GetBeaconsData(View):
 
@@ -110,27 +115,28 @@ class GetBeaconsData(View):
 
     def get(self, request):
  
-        start = request.GET['start']
-        end   = request.GET['end']
+        start    = request.GET['start']
+        end      = request.GET['end']
+        deviceId = request.GET['device']
 
         start = dateparse.parse_datetime(start)
         start = timezone.make_aware(start)
         end   = dateparse.parse_datetime(end)
         end   = timezone.make_aware(end)
-        
+
         beacons = Beacon.objects.filter(
-                beaconlog__time__range=(start, end)).distinct()
+                beaconlog__time__range=(start, end), 
+                beaconlog__device__externalId=deviceId).distinct()
 
         response = []
         for beacon in beacons:
             b = beacon.getDict()
-            records = BeaconLog.objects.filter(beacon=beacon, 
-                    time__range=(start, end))
+            records = BeaconLog.objects.filter(beacon=beacon,
+                    time__range=(start, end), device__externalId=deviceId)
             b['log'] = map(lambda x: x.getDict(), records)
             response.append(b)
 
         return JsonResponse(response, safe=False)
-
 
 
 class SaveData(View):
@@ -140,47 +146,52 @@ class SaveData(View):
 
     @method_decorator(csrf_exempt)
     def dispatch(self, request, *args, **kwargs):
-                return super(SaveData, self).dispatch(request, *args, **kwargs)
+        return super(SaveData, self).dispatch(request, *args, **kwargs)
 
     def post(self, request):
   
         jsonEncoded = request.POST['data']
         jsonDecoded = json.loads(jsonEncoded)
+        
+        deviceId = jsonDecoded['DeviceID']
+        beacons  = jsonDecoded['MeasureData']['Beacons']
+        events   = jsonDecoded['MeasureData']['Events']
 
-        beacons = jsonDecoded['Beacons']
-        events  = jsonDecoded['Events']
+        if DetectorDevice.objects.filter(externalId=deviceId).exists():
+            device = DetectorDevice.objects.get(externalId=deviceId)
+        else:
+            device = DetectorDevice.objects.create(externalId=deviceId)
 
         for beacon in beacons:
-            try:
-                b = Beacon.objects.get(macAddr=beacon['Mac Address'])
-            except Beacon.DoesNotExist:
-                b = Beacon(
+            if Beacon.objects.filter(macAddr=beacon['Mac Address']).exists():
+                beaconObj = Beacon.objects.get(macAddr=beacon['Mac Address'])
+            else:
+                beaconObj = Beacon.objects.create(
                     macAddr=beacon['Mac Address'],
                     uuid=beacon['UUID'],
                     major=beacon['Major'],
                     minor=beacon['Minor']
                     )
-                b.save()
 
             for record in beacon['Log']:
                 time = dateparse.parse_datetime(record['Time'])
                 time = timezone.make_aware(time)
-                r = BeaconLog(
+                r = BeaconLog.objects.create(
                         time=time,
                         rssi=record['RSSI'],
                         measurePower=record['Measure Power'],
-                        beacon=b
+                        beacon=beaconObj,
+                        device=device
                         )
-                r.save()
 
         for event in events:
             time = dateparse.parse_datetime(event['Time'])
             time = timezone.make_aware(time)
-            e = Event(
+            e = Event.objects.create(
                     time=time,
-                    event=event['Event']
+                    event=event['Event'],
+                    device=device
                     )
-            e.save()
 
         response = {}
         response['status'] = 'ok'
