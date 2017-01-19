88c28c487051b9223888bd107953a3d5d46feea8
server <cephei.1313@gmail.com>
2017-01-19 00:34:30 -0300

    now the url receives a deviceId

--- beacon/tests.py
+++ beacon/tests.py
@@ -43,53 +43,57 @@ class BeaconTestCase(TestCase):
     def setUp(self):
         """ save initial data on database """
 
+        self.deviceId = "94c97a7f526ade72"
         jsonData = """{
-        "Beacons":[
-         {"Mac Address":"[F7:BD:38:83:E5:83]",
-          "UUID":"7eccfcfa-f334-4042-9dc2-5b5432c33e06",
-          "Major":2,
-          "Minor":1,
-          "Log":[
-             {"Time":"2017-01-09 14:35:19","RSSI":-47,"Measure Power":-51},
-             {"Time":"2017-01-09 14:35:24","RSSI":-49,"Measure Power":-51},
-             {"Time":"2017-01-09 14:35:29","RSSI":-47,"Measure Power":-51},
-             {"Time":"2017-01-09 14:35:34","RSSI":-47,"Measure Power":-51},
-             {"Time":"2017-01-09 14:35:39","RSSI":-48,"Measure Power":-51},   
-             {"Time":"2017-01-09 14:35:44","RSSI":-49,"Measure Power":-51},   
-             {"Time":"2017-01-09 14:35:49","RSSI":-47,"Measure Power":-51},
-             {"Time":"2017-01-09 14:35:55","RSSI":-47,"Measure Power":-51},
-             {"Time":"2017-01-09 14:36:00","RSSI":-47,"Measure Power":-51},   
-             {"Time":"2017-01-09 14:36:05","RSSI":-48,"Measure Power":-51},
-             {"Time":"2017-01-09 14:36:10","RSSI":-47,"Measure Power":-51},
-             {"Time":"2017-01-09 14:36:15","RSSI":-46,"Measure Power":-51},
-             {"Time":"2017-01-09 14:36:20","RSSI":-47,"Measure Power":-51}
-          ]
-         },
-         {"Mac Address":"[E0:F4:4F:F4:98:69]",
-          "UUID":"7eccfcfa-f334-4042-9dc2-5b5432c33e06",
-          "Major":1,
-          "Minor":2,
-          "Log":[
-             {"Time":"2017-01-09 14:36:19","RSSI":-77,"Measure Power":-51},
-             {"Time":"2017-01-09 14:36:24","RSSI":-73,"Measure Power":-51},
-             {"Time":"2017-01-09 14:36:29","RSSI":-69,"Measure Power":-51},
-             {"Time":"2017-01-09 14:36:34","RSSI":-70,"Measure Power":-51},
-             {"Time":"2017-01-09 14:36:39","RSSI":-69,"Measure Power":-51},
-             {"Time":"2017-01-09 14:36:44","RSSI":-69,"Measure Power":-51},
-             {"Time":"2017-01-09 14:36:49","RSSI":-70,"Measure Power":-51},
-             {"Time":"2017-01-09 14:36:55","RSSI":-67,"Measure Power":-51},
-             {"Time":"2017-01-09 14:37:00","RSSI":-68,"Measure Power":-51},
-             {"Time":"2017-01-09 14:37:05","RSSI":-68,"Measure Power":-51},
-             {"Time":"2017-01-09 14:37:10","RSSI":-68,"Measure Power":-51},
-             {"Time":"2017-01-09 14:37:15","RSSI":-67,"Measure Power":-51},
-             {"Time":"2017-01-09 14:37:20","RSSI":-70,"Measure Power":-51}
-          ]
-         }
-        ],
-        "Events":[
-            {"Time":"2017-01-09 14:36:00","Event":"Esto es una prueba"},
-            {"Time":"2017-01-09 14:36:05","Event":"Esto es otra prueba"}
-        ]}"""
+        "DeviceID":"94c97a7f526ade72",
+        "MeasureData":{
+            "Beacons":[
+             {"Mac Address":"[F7:BD:38:83:E5:83]",
+              "UUID":"7eccfcfa-f334-4042-9dc2-5b5432c33e06",
+              "Major":2,
+              "Minor":1,
+              "Log":[
+                 {"Time":"2017-01-09 14:35:19","RSSI":-47,"Measure Power":-51},
+                 {"Time":"2017-01-09 14:35:24","RSSI":-49,"Measure Power":-51},
+                 {"Time":"2017-01-09 14:35:29","RSSI":-47,"Measure Power":-51},
+                 {"Time":"2017-01-09 14:35:34","RSSI":-47,"Measure Power":-51},
+                 {"Time":"2017-01-09 14:35:39","RSSI":-48,"Measure Power":-51},   
+                 {"Time":"2017-01-09 14:35:44","RSSI":-49,"Measure Power":-51},   
+                 {"Time":"2017-01-09 14:35:49","RSSI":-47,"Measure Power":-51},
+                 {"Time":"2017-01-09 14:35:55","RSSI":-47,"Measure Power":-51},
+                 {"Time":"2017-01-09 14:36:00","RSSI":-47,"Measure Power":-51},   
+                 {"Time":"2017-01-09 14:36:05","RSSI":-48,"Measure Power":-51},
+                 {"Time":"2017-01-09 14:36:10","RSSI":-47,"Measure Power":-51},
+                 {"Time":"2017-01-09 14:36:15","RSSI":-46,"Measure Power":-51},
+                 {"Time":"2017-01-09 14:36:20","RSSI":-47,"Measure Power":-51}
+               ]
+             },
+             {"Mac Address":"[E0:F4:4F:F4:98:69]",
+              "UUID":"7eccfcfa-f334-4042-9dc2-5b5432c33e06",
+              "Major":1,
+              "Minor":2,
+              "Log":[
+                 {"Time":"2017-01-09 14:36:19","RSSI":-77,"Measure Power":-51},
+                 {"Time":"2017-01-09 14:36:24","RSSI":-73,"Measure Power":-51},
+                 {"Time":"2017-01-09 14:36:29","RSSI":-69,"Measure Power":-51},
+                 {"Time":"2017-01-09 14:36:34","RSSI":-70,"Measure Power":-51},
+                 {"Time":"2017-01-09 14:36:39","RSSI":-69,"Measure Power":-51},
+                 {"Time":"2017-01-09 14:36:44","RSSI":-69,"Measure Power":-51},
+                 {"Time":"2017-01-09 14:36:49","RSSI":-70,"Measure Power":-51},
+                 {"Time":"2017-01-09 14:36:55","RSSI":-67,"Measure Power":-51},
+                 {"Time":"2017-01-09 14:37:00","RSSI":-68,"Measure Power":-51},
+                 {"Time":"2017-01-09 14:37:05","RSSI":-68,"Measure Power":-51},
+                 {"Time":"2017-01-09 14:37:10","RSSI":-68,"Measure Power":-51},
+                 {"Time":"2017-01-09 14:37:15","RSSI":-67,"Measure Power":-51},
+                 {"Time":"2017-01-09 14:37:20","RSSI":-70,"Measure Power":-51}
+              ]
+             }
+            ],
+            "Events":[
+                {"Time":"2017-01-09 14:36:00","Event":"Esto es una prueba"},
+                {"Time":"2017-01-09 14:36:05","Event":"Esto es otra prueba"}
+            ]}
+        }"""
  
         self.response = self.makePostRequest('save', {'data': jsonData})
 
@@ -109,7 +113,8 @@ class BeaconTestCase(TestCase):
         end   = '2017-01-09 14:36:01'
         url = 'getEventsData'
 
-        response = self.makeGetRequest(url, {'start':start,'end':end})
+        response = self.makeGetRequest(url, 
+                {'start':start,'end':end, 'device': self.deviceId})
 
         jsonResponse = json.loads(response.content)
         self.assertEqual(len(jsonResponse), 1)
@@ -122,10 +127,12 @@ class BeaconTestCase(TestCase):
         end   = '2017-01-09 14:37:25'
         url = 'getBeaconsData'
         
-        response = self.makeGetRequest(url, {'start':start,'end':end})
+        response = self.makeGetRequest(url, 
+                {'start':start,'end':end, 'device': self.deviceId})
 
         jsonResponse = json.loads(response.content)
 
+        #self.printJson(jsonResponse)
         self.assertEqual(len(jsonResponse), 1)
         self.assertEqual(len(jsonResponse[0]['log']), 5)
 
@@ -135,7 +142,8 @@ class BeaconTestCase(TestCase):
         end   = '2017-01-09 14:37:00'
         url = 'getChartData'
         
-        response = self.makeGetRequest(url, {'start':start,'end':end})
+        response = self.makeGetRequest(url, 
+                {'start':start,'end':end, 'device': self.deviceId})
 
         jsonResponse = json.loads(response.content)
       
